96e4fece2516fa38eb4b967debc434ce003d1722
Jim Barton <jameswbarton@gmail.com>
2024-02-21 16:58:04 -0500

    Revert "Outlook bg bug fix"
    This reverts commit f182d00caf25607f950925b897bca7f9dcbfdf19.

--- src/main.js
+++ src/main.js
@@ -38,32 +38,35 @@ function createSettings() {
 	// ALTERED VERSION OF SETCONTENT & SETDATA
 	// setContent(content, callback()). Only content attribute value is passed. It sets content stored in the widget as the original HTML(+script) of the body content of content block: CnC_Pickup_Wallet_ES_Omni in Test BU
 	sdk.setContent(
-		'<div style="color: #d4d4d4; font-family: courier; text-align: center; font-size: 12px;">'+
-		'%%[/******* Custom Ampscript Block and Email Settings ******/]%%'+
-		'</div>'+
-		'<script runat="server" language="ampscript">'+
-		'/* Email Settings */'+
-		'SET @Salutation = "' + salutation + '" /* for EN only - Hello, Hi, Dear, etc. FR uses Bonjour. */'+
-		'SET @subjectLine = "' + subjectLine + '" /* required for HTML archive; copy subject line here */'+
-		'SET @version = "' + version + '"'+
-		''+
-		'/* Footer Settings */'+
-		'SET @veeva = "' + veeva + '" /* add INS number here for footer */'+
-		'SET @Unsubscribe = "' + unsubscribe + '" /* Y/N toggles whether the unsub link displays - use N for transactional */'+
-		'SET @isi = "' + isi + '" /* Y/N toggles whether the footer displays the ISI link */'+
-		'SET @vib = "' + vib + '" /* Y/N toggles whether the View In Browser link displays */'+
-		'SET @OP5FAQ = "' + OP5FAQ + '" /* N or null uses the old FAQ in the footer, Y uses the OP5-specific FAQ page */'+
-		'/*enter  copyright text to display in footer */'+
-		'SET @customCopyright = "' + customCopyright + '"'+
-		''+
-		'/* UTMs - for use in template links; populate based on provided UTMs/links; utm_medium hardcoded as "email" */'+
-		'SET @ctoken = "' + ctoken + '"'+
-		'SET @utm_campaign = "' + utm_campaign + '"'+
-		'SET @utm_source = "' + utm_source + '"'+
-		'SET @utm_content = "' + utm_content + '"'+
-		'/* add @utm_string to any URL to tag with UTM string; you can also just hardcode the URLs manually */'+
-		'/* when you add @utm_string, youll need to add a ? or & depending on whether there is already a parameter string */'+
-		'</script>'
+		'<div>' +
+		'<!--[if mso]>' +
+		'<table align="' + alignment + '" style=" padding: ' + padding_top + 'px 32px ' + padding_bottom + 'px 32px;" bgcolor="' + block_color + '">' +
+		'<v:roundrect xmlns:v="urn:schemas-microsoft-com:vml" xmlns:w="urn:schemas-microsoft-com:office:word" style="line-height:45px;v-text-anchor:middle;width:' + vml_width + '; height:55px;" arcsize="75%"  fillcolor="' + button_hex + '" strokecolor="' + vml_border + '">' +
+		'<w:anchorlock>' +
+			'<center>' +
+			'<!--[endif]---->' +
+		'<div style="display:none;visibility:hidden;overflow:hidden;max-height:0px;width:' + vml_width + ';">' +
+			'<a alias="btn_' + button_text + '" href="' + button_url + '" style="color: '+ text_color + ';font-family: arial,sans-serif; font-size:16px; font-weight:bold; text-decoration:none;">' + button_text + '</a></div>' +
+		'<!--[if mso]>' +
+		'</center>' +
+		'</v:roundrect>' +
+		'</table>' +
+		'<![endif]-->' +
+		'</div>' +
+		'<!--[if !mso]-->' +
+		'<table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">' +
+		'<tr>' +
+			'<td align="' + alignment + '" class="mobile-padding" style=" padding: ' + padding_top + 'px 32px ' + padding_bottom + 'px 32px; background-color: ' + block_color + '">' +
+			'<table border="0" cellpadding="0" cellspacing="0" role="presentation">' +
+				'<tr>' +
+				'<td bgcolor="' + button_hex + '" style="padding: 0; font-size: 16px; font-weight: bold; font-family: Arial, helvetica, sans-serif; color: ' + text_color + '; text-align: left;border-radius: 30px; -moz-border-radius: 30px; -webkit-border-radius: 30px; text-align: center; background-color: ' + button_hex + ';padding-left: 32px; padding-right: 32px; border: ' + border_style + ';">' +
+					'<a alias="btn_' + button_text + '" conversion="false" data-linkto="other" href="' + button_url + '" style="color:' + text_color + ' !important;text-decoration:none; display: block; background-color: ' + button_hex + ';border: 0px;line-height: 40px;" target="_blank" title="">' + button_text + '</a></td>' +
+				'</tr>' +
+			'</table>' +
+			'</td>' +
+		'</tr>' +
+		'</table>' +
+		'<!--[endif]-->'
 		);
 
 	sdk.setSuperContent (
